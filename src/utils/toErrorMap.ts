4493aa1e7185fc67e6f8b3c64d9f61f5816cb8a5
MedEBoudali <med.sneaky@gmail.com>
2020-11-08 23:48:32 +0000

    commenting

--- src/utils/toErrorMap.ts
+++ src/utils/toErrorMap.ts
@@ -1,5 +1,6 @@
 import { FieldError } from '../generated/graphql';
 
+// Convert error array to object
 export const toErrorMap = (errors: FieldError[]) => {
   const errorMap: Record<string, string> = {};
   errors.forEach(({ field, message }) => (errorMap[field] = message));
