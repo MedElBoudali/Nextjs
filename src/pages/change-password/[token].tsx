40d82ee35abe3daf448b64d8190d5575af7921be
MedElBoudali <med.sneaky@gmail.com>
2020-11-16 15:53:08 +0000

    add change password page

---
+++ src/pages/change-password/[token].tsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+interface ChangePasswordProps {}
+
+const ChangePassword: React.FC<ChangePasswordProps> = props => {
+  return <div>This is ChangePassword Component/Page</div>;
+};
+
+ChangePassword.propTypes = {};
+
+export default ChangePassword;
