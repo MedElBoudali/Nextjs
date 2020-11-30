d605a2b8cfa341069d0d4359167139dc70a1243d
MedElBoudali <med.sneaky@gmail.com>
2020-11-30 20:58:49 +0000

    add navbar

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -4,6 +4,7 @@ import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
 import { useForgotPasswordMutation } from '../generated/graphql';
 import { useState } from 'react';
+import Navbar from '../components/layouts/Navbar';
 
 interface ForgotPAsswordProps {}
 
@@ -11,36 +12,39 @@ const ForgotPAssword: React.FC<ForgotPAsswordProps> = () => {
   const [forgotPassword] = useForgotPasswordMutation();
   const [isSend, setIsSend] = useState(false);
   return (
-    <Wrapper variant='small'>
-      <Formik
-        initialValues={{ email: '' }}
-        onSubmit={async values => {
-          const response = await forgotPassword({ variables: { email: values.email } });
-          if (response.data?.forgotPassword) {
-            setIsSend(true);
-          }
-        }}>
-        {({ isSubmitting }) => (
-          <div>
-            {isSend ? (
-              <Box>
-                <h4>✔️ Email Send!</h4>
-                <p> please check your email for reseting your password.</p>
-              </Box>
-            ) : (
-              <Form>
+    <>
+      <Navbar />
+      <Wrapper variant='small'>
+        <Formik
+          initialValues={{ email: '' }}
+          onSubmit={async values => {
+            const response = await forgotPassword({ variables: { email: values.email } });
+            if (response.data?.forgotPassword) {
+              setIsSend(true);
+            }
+          }}>
+          {({ isSubmitting }) => (
+            <div>
+              {isSend ? (
                 <Box>
-                  <InputField name='email' placeholder='name@example.com' label='Email' />
+                  <h4>✔️ Email Send!</h4>
+                  <p> please check your email for reseting your password.</p>
                 </Box>
-                <Button mt={2} type='submit' isLoading={isSubmitting} variantColor='teal'>
-                  Reset Password
-                </Button>
-              </Form>
-            )}
-          </div>
-        )}
-      </Formik>
-    </Wrapper>
+              ) : (
+                <Form>
+                  <Box>
+                    <InputField name='email' placeholder='name@example.com' label='Email' />
+                  </Box>
+                  <Button mt={2} type='submit' isLoading={isSubmitting} variantColor='teal'>
+                    Reset Password
+                  </Button>
+                </Form>
+              )}
+            </div>
+          )}
+        </Formik>
+      </Wrapper>
+    </>
   );
 };
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,58 +6,63 @@ import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import { useRegisterMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
-import { createUrqlClient } from '../utils/createUrqlClient';
-import { withUrqlClient } from 'next-urql';
+import Navbar from '../components/layouts/Navbar';
 
-interface registerProps {}
-
-const Register: React.FC<registerProps> = () => {
+const Register: React.FC<{}> = () => {
   const router = useRouter();
   const [register] = useRegisterMutation();
 
   return (
-    <Wrapper variant='small'>
-      <Formik
-        initialValues={{ username: '', email: '', password: '' }}
-        onSubmit={async (values, { setErrors }) => {
-          // send values (usernam, password) to our register mutation
-          const response = await register({ variables: { userInputs: values } });
-          if (response.data?.register.errors) {
-            // check if we have errors
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
-            // worked
-            router.push('/');
-          }
-        }}>
-        {({ isSubmitting }) => (
-          <Form>
-            <Box>
-              <InputField name='username' placeholder='username' label='Username' />
-            </Box>
-            <Box mt={2}>
-              <InputField name='email' placeholder='name@example.com' label='Email' />
-            </Box>
-            <Box mt={2}>
-              <InputField name='password' placeholder='password' label='Password' type='password' />
-            </Box>
-            <Box mt={2}>
-              <NextLink href='/forgot-password'>
-                <Link>Forgot your password?</Link>
-              </NextLink>
-            </Box>
-            <Box mt={2}>
-              <NextLink href='/login'>
-                <Link>You have an account? Click here to login.</Link>
-              </NextLink>
-            </Box>
-            <Button mt={2} type='submit' isLoading={isSubmitting} variantColor='teal'>
-              Register
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+    <>
+      <Navbar />
+      <Wrapper variant='small'>
+        <Formik
+          initialValues={{ username: '', email: '', password: '' }}
+          onSubmit={async (values, { setErrors }) => {
+            // send values (usernam, password) to our register mutation
+            const response = await register({ variables: { userInputs: values } });
+            if (response.data?.register.errors) {
+              // check if we have errors
+              setErrors(toErrorMap(response.data.register.errors));
+            } else if (response.data?.register.user) {
+              // worked
+              router.push('/');
+            }
+          }}>
+          {({ isSubmitting }) => (
+            <Form>
+              <Box>
+                <InputField name='username' placeholder='username' label='Username' />
+              </Box>
+              <Box mt={2}>
+                <InputField name='email' placeholder='name@example.com' label='Email' />
+              </Box>
+              <Box mt={2}>
+                <InputField
+                  name='password'
+                  placeholder='password'
+                  label='Password'
+                  type='password'
+                />
+              </Box>
+              <Box mt={2}>
+                <NextLink href='/forgot-password'>
+                  <Link>Forgot your password?</Link>
+                </NextLink>
+              </Box>
+              <Box mt={2}>
+                <NextLink href='/login'>
+                  <Link>You have an account? Click here to login.</Link>
+                </NextLink>
+              </Box>
+              <Button mt={2} type='submit' isLoading={isSubmitting} variantColor='teal'>
+                Register
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Wrapper>
+    </>
   );
 };
 
